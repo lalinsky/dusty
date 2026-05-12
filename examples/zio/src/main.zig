d22d8bff53f1201a794025915d05d36e860dd2d7
Lukas Lalinsky <lukas@lalinsky.com>
2026-05-12 11:15:42 +0200

    Add standalone zio example

---
+++ examples/zio/src/main.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+const zio = @import("zio");
+const http = @import("dusty");
+
+fn handleRoot(_: *http.Request, res: *http.Response) !void {
+    res.body = "Hello World!\n";
+}
+
+pub fn main(init: std.process.Init) !void {
+    var rt = try zio.Runtime.init(init.gpa, .{});
+    defer rt.deinit();
+
+    var server = http.Server(void).init(init.gpa, rt.io(), .{}, {});
+    defer server.deinit();
+
+    server.router.get("/", handleRoot);
+
+    const addr: http.Address = .{ .ip = try std.Io.net.IpAddress.parse("127.0.0.1", 8080) };
+    std.log.info("Starting server on http://127.0.0.1:8080", .{});
+    try server.listen(addr);
+}
